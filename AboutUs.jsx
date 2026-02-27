4c97bca2055ff55a1e56eff2c60390d571ed8e94
zehor-l <64021448+zehor-l@users.noreply.github.com>
2026-02-27 14:20:42 +0100

    Add AboutUs component for nursery description

---
+++ AboutUs.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div className="about-us">
+      <h2>About Paradise Nursery</h2>
+      <p>
+        Paradise Nursery is a premium online plant shop offering a variety of
+        houseplants to brighten your home. Our goal is to provide quality plants
+        with excellent customer service and a seamless shopping experience.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
